7ef5519043b4fc3ea83085b154ac8bb34511703e
伊藤淳 <itojun@itojunMacBook-Pro.local>
2017-09-30 21:49:17 +0900

    EXAM2 update 20170930

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -1,7 +1,17 @@
 module PicturesHelper
   def photo_img(picture)
-    return image_tag(picture.image, alt: picture.user.name) if picture.image?
     if picture.image?
+
+      image = MiniMagick::Image.open(picture.image.file.file)
+
+      wdth = image[:width]
+      hght = image[:height]
+
+      if wdth > 1000 && hght > 500
+        image.resize "600x400"
+        image.write picture.image.file.file
+      end
+
       image_tag(picture.image, alt: picture.user.name)
     else
       return;
